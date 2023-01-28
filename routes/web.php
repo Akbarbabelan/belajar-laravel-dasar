80f0ce2cbfb4183e1b12e805882439458e66f851
Akbarbabelan <babelan96@gmail.com>
2023-01-28 23:20:16 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pzn', function () {
+    return "Hello Programmer Zaman Now";
+});
+
+Route::redirect('/youtobe', '/pzn');
+
+Route::fallback(function (){
+    return "404 by Programmer Zaman Now";
+});
